76041010b0942f7128d83b88f6efbe4e8b8c9953
Nikolay Mavrenkov <koluch@koluch.ru>
2013-02-28 02:05:10 +0400

    Make possible to read part of tag in any order (e. g. header after frames)

--- src/id3v2_stream_reader.erl
+++ src/id3v2_stream_reader.erl
@@ -16,14 +16,14 @@ test(FileName) ->
     io:format("Stream~n", []),
     Srv = start(Stream),
     io:format("Read~n", []),
-    Header = get_header(Srv),
-    io:format("Header: ~p~n", [Header]),
     TPE1 = get_frame(Srv, "TPE1"),
     io:format("Frame TPE1: ~p~n", [TPE1]),
     TIT2 = get_frame(Srv, "TIT2"),
     io:format("Frame TIT2: ~p~n", [TIT2]),
     Wrong = get_frame(Srv, "WRNG"),
     io:format("Wrong: ~p~n", [Wrong]),
+    Wrong2 = get_frame(Srv, "WRNG"),
+    io:format("Wrong: ~p~n", [Wrong2]),
     TALB = get_frame(Srv, "TALB"),
     io:format("Frame TALB: ~p~n", [TALB]),
     stop(Srv)
@@ -37,35 +37,41 @@ loop({Stream, Tag}) ->
     receive
 
         {From, {get_header}} ->
-            NewTag = case Tag#id3_tag.header of
-                undefined ->
-                    Header = read_header(Stream),
-                    Tag#id3_tag{header = Header};
-                _ -> Tag
-            end,
-            From ! {self(), NewTag#id3_tag.header},
-            loop({Stream, NewTag});
+            UpdTag = update_tag_fill_header(Tag, Stream),
+            From ! {self(), UpdTag#id3_tag.header},
+            loop({Stream, UpdTag});
+
+        {From, {get_ext_header}} ->
+            UpdTag = update_tag_fill_header(Tag, Stream), %% Make sure, that all previous information is readed
+            UpdTag2 = update_tag_fill_ext_header(UpdTag, Stream),
+            From ! {self(), UpdTag2#id3_tag.ext_header},
+            loop({Stream, UpdTag2});
 
         {From, {get_frame, Id}} ->
-            case find_readed(Tag#id3_tag.frames, Id) of
+            UpdTag = update_tag_fill_header(Tag, Stream), %% Make sure, that all previous information is readed
+            UpdTag2 = update_tag_fill_ext_header(UpdTag, Stream),
+            case find_readed(UpdTag2#id3_tag.frames, Id) of
                 {ok, Frame} ->
                     From ! {self(), Frame},
-                    loop({Stream, Tag});
+                    loop({Stream, UpdTag2});
                 _ ->
                     case find_frame(Stream, Tag#id3_tag.frames, Id) of
                         {ok, {Frame, NewFrames}} ->
                             From ! {self(), Frame},
-                            NewTag = Tag#id3_tag{frames = NewFrames},
+                            NewTag = UpdTag2#id3_tag{frames = NewFrames},
                             loop({Stream, NewTag});
                         {error, {Why, NewFrames}} ->
                             From ! {self(), {error, Why}},
-                            NewTag = Tag#id3_tag{frames = NewFrames},
+                            NewTag = UpdTag2#id3_tag{frames = NewFrames},
                             loop({Stream, NewTag})
                     end
             end;
 
-        {From, {stop}} ->
-             io:format("Bye!~n");
+        {_From, {stop}} ->
+            io:format("Bye!~n");
+
+        {_From, {new_stream, NewStream}} ->
+            loop({NewStream, #id3_tag{}});
 
         Other ->
             io:format("Unknown message: ~p~n", [Other]),
@@ -73,6 +79,26 @@ loop({Stream, Tag}) ->
 
     end,
     loop({Stream, Tag}).
+
+
+update_tag_fill_header(Tag, Stream) ->
+    case Tag#id3_tag.header of
+        undefined ->
+            Header = read_header(Stream),
+            Tag#id3_tag{header = Header};
+        _ -> Tag
+    end.
+
+update_tag_fill_ext_header(Tag, Stream) ->
+    case Tag#id3_tag.ext_header of
+        undefined ->
+            ExtHeader = case Tag#id3_tag.header#id3_header.flags#id3_header_flags.ext_header of
+                true -> read_ext_header(Stream);
+                _ -> not_present
+            end,
+            Tag#id3_tag{ext_header = ExtHeader};
+        _ -> Tag
+    end.
 
 %% Found tag within already readed
 find_readed(undefined, _Id) -> {error,not_found};
@@ -114,6 +140,29 @@ read_header(Stream) ->
             end;
          Other -> io:format("Other output: ~p~n", [Other])
     end.
+
+read_ext_header(Stream) ->
+    case stream:read(Stream,6) of
+        {ok, <<SyncsafeSize:32,32#01:8,BinFlags/binary>>} ->
+            {Update,CRC,Rests} = case BinFlags of
+                <<2#0:1,_Update:1,_CRC:1,_Rests:1,2#0000:4>> ->
+                    {int_to_bool(_Update),int_to_bool(_CRC),int_to_bool(_Rests)}
+            end,
+            UpdateFlag = read_ext_header_flag(Stream, update, Update),
+            CRCFlag = read_ext_header_flag(Stream, crc, CRC),
+            RestsFlag = read_ext_header_flag(Stream, rests, Rests),
+            #id3_ext_header{
+                    size = read_syncsafe_int(SyncsafeSize),
+                    flags = [UpdateFlag,CRCFlag,RestsFlag]
+            }
+    end.
+
+read_ext_header_flag(File, Name, true) ->
+    {ok, <<FlagLength>>} = file:read(File, 1),
+    {ok, FlagData} = file:read(File, FlagLength),
+    #id3_ext_header_flag{name = Name, value = true, data = FlagData};
+read_ext_header_flag(_File, Name, _False) -> #id3_ext_header_flag{name = Name, value = false}.
+
 
 read_frame(Stream) ->
     {ok, <<BinId:4/binary,SyncsafeSize:32,Flags:2/binary>>} = stream:read(Stream, 10),
